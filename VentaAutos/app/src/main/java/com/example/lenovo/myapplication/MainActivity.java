09bf6272d42d29cca9f4089bef490f2483be47f0
andrestein <andresgrandapiedrahita@gmail.com>
2016-03-01 06:54:18 -0500

    interfaz de primera actividad terminada

--- VentaAutos/app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
+++ VentaAutos/app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
@@ -1,52 +1,61 @@
 package com.example.lenovo.myapplication;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText txtPrecio;
+    private Button btnCalcular;
+    private RadioButton radioBMW,radioAudi,radioToyota;
+    private CheckBox checkSunroof,checkNitro,checkPolarizado;
+    private RadioGroup rg;
+    private double precio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        initComponents();
+        autoSeleccionado();
+        
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void autoSeleccionado(){
+        if(rg.getCheckedRadioButtonId()==R.id.radioBtnBMW){
+            precio =40000;
+            txtPrecio.setText(""+precio);
+        }else if(rg.getCheckedRadioButtonId() == R.id.radioBtnAudi){
+            precio =40000;
+            txtPrecio.setText(""+precio);
+        }else if(rg.getCheckedRadioButtonId() == -1){
+            Toast.makeText(getApplicationContext(), "Necesita seleccionar una marca de auto",Toast.LENGTH_SHORT);
         }
+        else{
+            precio =40000;
+            txtPrecio.setText(""+precio);
+        }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void initComponents(){
+        precio =0;
+        rg= (RadioGroup)findViewById(R.id.rg);
+        radioBMW = (RadioButton)findViewById(R.id.radioBtnBMW);
+        radioAudi= (RadioButton)findViewById(R.id.radioBtnAudi);
+        radioToyota= (RadioButton)findViewById(R.id.radioBtnToyota);
+        checkSunroof=(CheckBox)findViewById(R.id.checkSunroof);
+        checkNitro=(CheckBox)findViewById(R.id.checkNitro);
+        checkPolarizado=(CheckBox)findViewById(R.id.checkPolarizado);
+        txtPrecio = (EditText)findViewById(R.id.txtPecio);
+        btnCalcular = (Button)findViewById(R.id.btnCalcular);
     }
 }
